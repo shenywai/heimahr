9141090e86703c7439125059e0dc180d4b81f44d
gao_y <1422149692@qq.com>
2024-03-05 15:36:28 +0800

    区分生产环境,将生产环境进行灵活化

--- src/utils/request.js
+++ src/utils/request.js
@@ -3,7 +3,7 @@ import store from '@/store'
 import { Message } from 'element-ui'
 
 const service = axios.create({
-  baseURL: '/api', // 基地址
+  baseURL: process.env.VUE_APP_BASE_API, // 基地址
   timeout: 10000 // 超时时间
 })
 
